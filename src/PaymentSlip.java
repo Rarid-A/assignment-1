0f5f1b0b09a70fb22ca41d86b2a31b9bea8eeb97
Rarid_A <ra8475al-s@student.lu.se>
2024-11-06 13:34:58 +0100

    first commit

--- src/PaymentSlip.java
+++ src/PaymentSlip.java
@@ -2,19 +2,39 @@ import java.text.DecimalFormat;
 
 public class PaymentSlip {
     // Attributes: name, hourlyRate, hoursWorked
-    private final String name;
-    private final int hourlyRate;
-    private final int hoursWorked;
-
+    private String name;
+    private int hourlyRate;
+    private int hoursWorked;
+    private final double TAX_RATE = 0.30;
+    //Constructor without paramaters
+    public PaymentSlip() {
+    
+    }
     // Constructor to initialize the attributes
     public PaymentSlip(String name, int hourlyRate, int hoursWorked) {
         this.name = name;
         this.hourlyRate = hourlyRate;
         this.hoursWorked = hoursWorked;
     }
-// getter and setter methods for the name attribute
+// getter and setter methods for the all attribute
     public String getName() {
         return name;
+    
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+    public void setHourlyRate(int hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
     }
     // Method to calculate the salary before tax
     public int calculateSalary() {
@@ -24,7 +44,7 @@ public class PaymentSlip {
     // Method to calculate net salary after 30% tax
         public String calculateNetSalary() {
             double salary = calculateSalary();
-            double netSalary = salary - (salary * 0.30);
+            double netSalary = salary - (salary * TAX_RATE);
             DecimalFormat df = new DecimalFormat("##.#");
             return df.format(netSalary);
     }
